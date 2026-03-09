1fca6be353f80df358a2c21357ec8bd69fe5f031
usmanzm-27 <mohammedusmanzm@gmail.com>
2026-03-09 13:33:59 +0000

    Assignment1 on Arrays Submission

--- Assignment1.java
+++ Assignment1.java
@@ -1,16 +1,29 @@
-class Assignment1 {
-  public static int[] findLeapYears(int year) {
-  // Implement your code here and change the return value accordingly
-    
-  return null;
-  }
-  
-  public static void main(String[] args) {
-    int year = 2000;
-    int[] leapYears;
-    leapYears = findLeapYears(year);
-    for (int index = 0; index < leapYears.length; index++) {
-      System.out.println(leapYears[index]);
+class Assignment1{
+
+    public static int[] findLeapYears(int year) {
+        int[] leapYears = new int[15];
+        int count = 0;
+
+        while (count < 15) {
+
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                leapYears[count] = year;
+                count++;
+            }
+
+            year++;
+        }
+
+        return leapYears;
     }
-  }
+
+    public static void main(String[] args) {
+        int year = 2000;   // Sample Input
+
+        int[] result = findLeapYears(year);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
 }
